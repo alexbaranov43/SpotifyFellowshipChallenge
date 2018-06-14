4a812b5fdef24193d77d9abb3b1fac0f14d96bc4
alexbaranov43 <alexbaranov43@gmail.com>
2018-06-14 16:23:28 -0400

    question 3 ready

---
+++ question3.js
@@ -0,0 +1,17 @@
+changePosibilities = (amount, denominations) => {
+let possibilties = new Array(amount + 1).fill(0)
+possibilties[0] = 1;
+denominations.forEach(denomination => {
+    for(i = denomination; i <=amount; i++){
+        let remainder = i - denomination;
+        possibilties[i] += possibilties[remainder]
+    }
+    
+});
+
+console.log(possibilties[amount]);
+
+}
+
+changePosibilities(6, [1,2,3,4])
+changePosibilities(4, [1,2,3,4])
